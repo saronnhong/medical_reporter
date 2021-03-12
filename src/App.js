10e59885d5ff4569afad3e52e982c5ff236f0269
Saron <saronnhong@gmail.com>
2021-03-12 15:51:57 -0800

    restored old version on new git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,185 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import SearchBar from './components/SearchBar';
+import Reports from './components/Reports';
 import './App.css';
+import { BOOKS } from "./books/books";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    library: [],
+    search: '',
+    searchResults: BOOKS,
+    excludedWords: '',
+    isSearchOptionsDisplay: false,
+    isDisplayTxtFile: false,
+    idToPass: 0,
+    isDisplayTable: true,
+    tags: {
+      goodreport: [1,2,3,4],
+      conditionpresent: [0, 4]
+    }
+  }
+  componentDidMount() {
+    this.createDictionary()
+  }
+
+  createDictionary = () => {
+    let tempState = [];
+    for (let i = 0; i < BOOKS.length; i++) {
+      let fileContents = BOOKS[i].location;
+      // let fileAuthor = BOOKS[i].author;
+      // let fileTitle = BOOKS[i].title;
+      let fileId = BOOKS[i].id;
+      let test = fileContents.toLowerCase().replace(/[^A-Za-z' ;]/g, "").split(' ');
+
+      let hash = {};
+      for (let j = 0; j < test.length; j++) {
+        if (!hash[test[j]]) {
+          hash[test[j]] = 1;
+        } else {
+          hash[test[j]]++;
+        }
+      }
+      let currentStorage = { dictionary: hash, id: fileId, location: fileContents }
+      tempState.push(currentStorage);
+    }
+    this.setState({ library: tempState });
+  }
+  searchBooks = () => {
+    const { library } = this.state;
+    const { search } = this.state;
+    const { excludedWords } = this.state;
+
+    let searchItems = search.toLowerCase().split(' ');
+    let excludedItems = excludedWords.toLowerCase().split(' ');
+    let searchResultsItems = [];
+    for (let book in library) {
+      let wordChecker = true;
+      for (let k = 0; k < searchItems.length; k++) {
+        if (!library[book].dictionary[searchItems[k]]) {
+          wordChecker = false;
+        }
+      }
+      if (excludedWords.length > 0) {
+        for (let l = 0; l < excludedItems.length; l++) {
+          if (library[book].dictionary[excludedItems[l]]) {
+            wordChecker = false;
+          }
+        }
+      }
+      if (wordChecker === true) {
+        searchResultsItems.push(library[book]);
+      }
+    }
+    this.setState({ ...this.state, searchResults: searchResultsItems, isDisplayTable: true, isDisplayTxtFile: false }, () => console.log(this.state.searchResults));
+  }
+  handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      this.searchBooks();
+    }
+  }
+
+  onClickBookRow = (book) => {
+    this.setState({ ...this.state, isDisplayTxtFile: true, idToPass: book.id, isDisplayTable: false })
+  }
+
+  onClickSearchDropDown = () => {
+    this.setState({ isSearchOptionsDisplay: !this.state.isSearchOptionsDisplay })
+  }
+  onChangeSearchInput = (e) => {
+    this.setState({ search: e.target.value })
+  }
+  onChangeExcludeInput = (e) => {
+    this.setState({ excludedWords: e.target.value })
+  }
+
+  displayTagResult = item => {
+    const { tags } = this.state;
+    let tempState=[];
+    let currentTag = tags[item];
+    for(let book in BOOKS){
+      for(let i=0; i<currentTag.length; i++){
+        if(BOOKS[book].id === currentTag[i]){
+          tempState.push(BOOKS[book]);
+        }
+      }
+    }
+    console.log(tempState);
+    this.setState({ ...this.state, searchResults: tempState, isDisplayTable: true, isDisplayTxtFile: false }, ()=> console.log(this.state.searchResults));
+  }
+
+  displayInbox = () => {
+    this.setState({ ...this.state, searchResults: BOOKS, isDisplayTable: true, isDisplayTxtFile: false });
+  }
+
+  addTag = (item, id) => {
+    const { tags } = this.state;
+    let currentTag = tags[item];
+    if(!currentTag.includes(id)){
+      currentTag.push(id)
+    }
+    this.setState({ ...this.state, tags: {...tags, item: currentTag}});
+  }
+  removeTag = (item, id) => {
+    const { tags } = this.state;
+    let currentTag = tags[item];
+    let newArr=[];
+    if(currentTag.includes(id)){
+      newArr= currentTag.filter(word => word !== id)
+      // console.log(newArr);
+    }
+    if(item === "goodreport"){
+      this.setState({...this.state, tags: {...tags, "goodreport": newArr}});
+    }else if(item === "conditionpresent"){
+      this.setState({...this.state, tags: {...tags, "conditionpresent": newArr}});
+    }
+    
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-2" style={{ marginTop: "150px", padding: '20px' }}>
+              <div className="row">
+                <div className="tags" onClick={()=> this.displayInbox()}><i class="fas fa-envelope-open-text"></i> Inbox</div>
+                <div className="tags" onClick={()=> this.displayTagResult("goodreport")}><i class="fas fa-hashtag"></i>goodreport</div>
+                <div className="tags" onClick={()=> this.displayTagResult("conditionpresent")}><i class="fas fa-hashtag"></i>conditionpresent</div>
+              </div>
+
+            </div>
+            <div className="col-md-10">
+              <div className="row">
+                <div className="col-md-12">
+                  <h1 className="appTitle text-center">Medical Reports Searcher</h1>
+                </div>
+              </div>
+              <SearchBar
+                isSearchOptionsDisplay={this.state.isSearchOptionsDisplay}
+                onClickSearchDropDown={this.onClickSearchDropDown}
+                onClickSearchBtn={this.searchBooks}
+                onChangeSearchInput={this.onChangeSearchInput}
+                onChangeExcludeInput={this.onChangeExcludeInput}
+                handleKeyDown={this.handleKeyDown}
+              />
+              <Reports
+                isDisplayTable={this.state.isDisplayTable}
+                searchResults={this.state.searchResults}
+                isDisplayTxtFile={this.state.isDisplayTxtFile}
+                idToPass={this.state.idToPass}
+                onClick={this.onClickBookRow}
+                tags={this.state.tags}
+                addTag={this.addTag}
+                removeTag={this.removeTag}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/books/books.js
@@ -0,0 +1,259 @@
+import raw from "raw.macro";
+
+export const BOOKS = [
+    {
+        id: 0,
+        location: raw('./Warrior_Maid_Of_Mars.txt'),
+        url: process.env.PUBLIC_URL + '/books/Warrior_Maid_Of_Mars.txt'
+    },
+    {
+        id: 1,
+        location: raw('./The_Rocketeers_Have_Shaggy_Ears.txt'),
+        url: process.env.PUBLIC_URL + '/books/The_Rocketeers_Have_Shaggy_Ears.txt'
+    },
+    {
+        id: 2,
+        location: raw('./He_That_Will_Not_When_He_May.txt'),
+        url: process.env.PUBLIC_URL + '/books/He_That_Will_Not_When_He_May.txt'
+    },
+    {
+        id: 3,
+        location: raw('./Some_Historic_Trees.txt'),
+        url: process.env.PUBLIC_URL + '/books/Some_Historic_Trees.txt'
+    },
+    {
+        id: 4,
+        location: raw('./The_Last_Two_Alive.txt'),
+        url: process.env.PUBLIC_URL + '/books/The_Last_Two_Alive.txt'
+    },
+    {
+        id: 5,
+        location: raw('./Airman.txt'),
+        url: process.env.PUBLIC_URL + '/books/Airman.txt'
+    },
+    {
+        id: 6,
+        location: raw('./Ticket_To_Paradise.txt'),
+        url: process.env.PUBLIC_URL + '/books/Ticket_To_Paradise.txt'
+    },
+    {
+        id: 7,
+        location: raw('./Lord_Of_The_Silent_Death.txt'),
+        url: process.env.PUBLIC_URL + '/books/Lord_Of_The_Silent_Death.txt'
+    },
+    {
+        id: 8,
+        location: raw('./Brattons_Idea.txt'),
+        url: process.env.PUBLIC_URL + '/books/Brattons_Idea.txt'
+    },
+    {
+        id: 9,
+        location: raw('./The_Secret_Dispatch.txt'),
+        url: process.env.PUBLIC_URL + '/books/The_Secret_Dispatch.txt'
+    },
+    {
+        id: 10,
+        location: raw('./The_Spanish_Influenze_Pandemic.txt'),
+        url: process.env.PUBLIC_URL + '/books/The_Spanish_Influenze_Pandemic.txt'
+    },
+    {
+        id: 11,
+        location: raw('./Mortal_Summer.txt'),
+        url: process.env.PUBLIC_URL + '/books/Mortal_Summer.txt'
+    },
+    {
+        id: 12,
+        location: raw('./Modest_Proposal.txt'),
+        url: process.env.PUBLIC_URL + '/books/Modest_Proposal.txt'
+    },
+    {
+        id: 13,
+        location: raw('./Sherlock_Holmes.txt'),
+        url: process.env.PUBLIC_URL + '/books/Sherlock_Holmes.txt'
+    },
+    {
+        id: 14,
+        location: raw('./Beowulf.txt'),
+        url: process.env.PUBLIC_URL + '/books/Beowulf.txt'
+    },
+    {
+        id: 15,
+        location: raw('./Odyssey.txt'),
+        url: process.env.PUBLIC_URL + '/books/Odyssey.txt'
+    },
+    {
+        id: 16,
+        location: raw('./Jungle_Book.txt'),
+        url: process.env.PUBLIC_URL + '/books/Jungle_Book.txt'
+    },
+    {
+        id: 17,
+        location: raw('./56463-0.txt'),
+        url: process.env.PUBLIC_URL + '/books/56463-0.txt'
+    },
+    {
+        id: 18,
+        location: raw('./Nursery_Rhymes.txt'),
+        url: process.env.PUBLIC_URL + '/books/Nursery_Rhymes.txt'
+    },
+    {
+        id: 19,
+        location: raw('./Memoirs_Of_Sherlock.txt'),
+        url: process.env.PUBLIC_URL + '/books/Memoirs_Of_Sherlock.txt'
+    },
+    {
+        id: 20,
+        location: raw('./Grimms_Fairy_Tales.txt'),
+        url: process.env.PUBLIC_URL + '/books/Grimms_Fairy_Tales.txt'
+    },
+    {
+        id: 21,
+        location: raw('./1292-0.txt'),
+        url: process.env.PUBLIC_URL + '/books/1292/1292-0.txt'
+    },
+    {
+        id: 22,
+        location: raw('./pg61963.txt'),
+        url: process.env.PUBLIC_URL + '/books/pg61963.txt'
+    },
+    {
+        id: 23,
+        location: raw('./pg1597.txt'),
+        url: process.env.PUBLIC_URL + '/books/pg1597.txt'
+    },
+    {
+        id: 24,
+        location: raw('./108-0.txt'),
+        url: process.env.PUBLIC_URL + '/books/108-0.txt'
+    },
+    {
+        id: 25,
+        location: raw('./pg27673.txt'),
+        url: process.env.PUBLIC_URL + '/books/pg27673.txt'
+    },
+    {
+        id: 26,
+        location: raw('./5225-0.txt'),
+        url: process.env.PUBLIC_URL + '/books/5225-0.txt'
+    },
+    {
+        id: 27,
+        location: raw('./pg18735.txt'),
+        url: process.env.PUBLIC_URL + '/books/pg18735.txt'
+    },
+    {
+        id: 28,
+        location: raw('./pg4081.txt'),
+        url: process.env.PUBLIC_URL + '/books/pg4081.txt'
+    },
+    {
+        id: 29,
+        location: raw('./57333-0.txt'),
+        url: process.env.PUBLIC_URL + '/books/57333-0.txt'
+    },
+    {
+        id: 30,
+        location: raw('./pg12030.txt'),
+        url: process.env.PUBLIC_URL + '/books/pg12030.txt'
+    },
+    {
+        id: 31,
+        location: raw('./49513-0.txt'),
+        url: process.env.PUBLIC_URL + '/books/49513-0.txt'
+    },
+    {
+        id: 32,
+        location: raw('./204-0.txt'),
+        url: process.env.PUBLIC_URL + '/books/204-0.txt'
+    },
+    {
+        id: 33,
+        location: raw('./pg2490.txt'),
+        url: process.env.PUBLIC_URL + '/books/pg2490.txt'
+    },
+    {
+        id: 34,
+        location: raw('./pg2383.txt'),
+        url: process.env.PUBLIC_URL + '/books/pg2383.txt'
+    },
+    {
+        id: 35,
+        location: raw('./pg8795.txt'),
+        url: process.env.PUBLIC_URL + '/books/pg8795.txt'
+    },
+    {
+        id: 36,
+        location: raw('./pg2428.txt'),
+        url: process.env.PUBLIC_URL + '/books/pg2428.txt'
+    },
+    {
+        id: 37,
+        location: raw('./pg56625.txt'),
+        url: process.env.PUBLIC_URL + '/books/pg56625.txt'
+    },
+    {
+        id: 38,
+        location: raw('./16452-0.txt'),
+        url: process.env.PUBLIC_URL + '/books/16452-0.txt'
+    },
+    {
+        id: 39,
+        location: raw('./pg5402.txt'),
+        url: process.env.PUBLIC_URL + '/books/pg5402.txt'
+    },
+    {
+        id: 40,
+        location: raw('./pg25609.txt'),
+        url: process.env.PUBLIC_URL + '/books/pg25609.txt'
+    },
+    {
+        id: 41,
+        location: raw('./1732-0.txt'),
+        url: process.env.PUBLIC_URL + '/books/1732-0.txt'
+    },
+    {
+        id: 42,
+        location: raw('./70-0.txt'),
+        url: process.env.PUBLIC_URL + '/books/70-0.txt'
+    },
+    {
+        id: 43,
+        location: raw('./pg16436.txt'),
+        url: process.env.PUBLIC_URL + '/books/pg16436.txt'
+    },
+    {
+        id: 44,
+        location: raw('./19481-0.txt'),
+        url: process.env.PUBLIC_URL + '/books/19481-0.txt'
+    },
+    {
+        id: 45,
+        location: raw('./pg2811.txt'),
+        url: process.env.PUBLIC_URL + '/books/pg2811.txt'
+    },
+    {
+        id: 46,
+        location: raw('./pg27424.txt'),
+        url: process.env.PUBLIC_URL + '/books/pg27424.txt'
+    },
+    {
+        id: 47,
+        location: raw('./53489-0.txt'),
+        url: process.env.PUBLIC_URL + '/books/53489-0.txt'
+    },
+    {
+        id: 48,
+        location: raw('./986-0.txt'),
+        url: process.env.PUBLIC_URL + '/books/986-0.txt'
+    },
+    {
+        id: 49,
+        location: raw('./pg1929.txt'),
+        url: process.env.PUBLIC_URL + '/books/pg1929.txt'
+    },
+    {
+        id: 50,
+        location: raw('./pg14568.txt'),
+        url: process.env.PUBLIC_URL + '/books/pg14568.txt'
+    },
+];
